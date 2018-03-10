085091e6827076e0877682e246646b5dc981deac
Richard Liaw <rliaw@berkeley.edu>
2018-03-10 13:21:55 -0800

    changes to setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,7 @@
-conda create env -n ulab python==3.6 -y
+conda create -n ulab python==3.6 -y
 source activate ulab
-conda install -c intel mkl -y
+conda install -c anaconda mkl -y
+conda install -c conda-forge jupyterlab -y
 conda install matplotlib -y
 conda install pytorch torchvision -c pytorch -y
 pip install -U pillow 
