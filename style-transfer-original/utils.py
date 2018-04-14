ac531d0d41506d67aa75b94ac3c35f5b62f90086
Richard Liaw <rliaw@berkeley.edu>
2018-04-14 13:11:14 -0700

    changes

---
+++ style-transfer-original/utils.py
@@ -0,0 +1,42 @@
+import time
+import os
+
+import torch
+import torchvision
+from torchvision import transforms
+import vgg as v
+
+image_dir = os.getcwd() + '/Images/'
+model_dir = os.getcwd() + '/Models/'
+img_size = 512
+
+prep = transforms.Compose([transforms.Resize(img_size),
+                           transforms.ToTensor(),
+                           transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to BGR
+                           transforms.Normalize(mean=[0.40760392, 0.45795686, 0.48501961], #subtract imagenet mean
+                                                std=[1,1,1]),
+                           transforms.Lambda(lambda x: x.mul_(255)),
+                          ])
+postpa = transforms.Compose([transforms.Lambda(lambda x: x.mul_(1./255)),
+                           transforms.Normalize(mean=[-0.40760392, -0.45795686, -0.48501961], #add imagenet mean
+                                                std=[1,1,1]),
+                           transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to RGB
+                           ])
+postpb = transforms.Compose([transforms.ToPILImage()])
+
+
+def postp(tensor): # to clip results in the range [0,1]
+    t = postpa(tensor)
+    t[t>1] = 1
+    t[t<0] = 0
+    img = postpb(t)
+    return img
+
+def load_vgg():
+    vgg = v.VGG()
+    vgg.load_state_dict(torch.load(model_dir + 'vgg_conv.pth'))
+    for param in vgg.parameters():
+        param.requires_grad = False
+    if torch.cuda.is_available():
+        vgg.cuda()
+    return vgg
